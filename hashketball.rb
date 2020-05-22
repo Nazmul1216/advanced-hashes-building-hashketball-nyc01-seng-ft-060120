3e802d20eb36dc6b7263b086148890c9d7ae9fe6
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-22 20:17:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,14 +4,32 @@ def game_hash
     :home=>{
       :team_name=>"",
       :colors=>[],
-      :players=>[]
+      :players=>[
+        {
+        :player_name=> ,
+        :number=> ,
+        :shoe=> ,
+        :points=> ,
+        :rebounds=> ,
+        :assists=> ,
+        :steals=> ,
+        :blocks=> ,
+        :slam_dunks=> ,
+      }]
     },
     :away=>{
       :team_name=>"",
       :colors=>[],
       :players=>[{
-        player_name:
-        
+        :player_name=> ,
+        :number=> ,
+        :shoe=> ,
+        :points=> ,
+        :rebounds=> ,
+        :assists=> ,
+        :steals=> ,
+        :blocks=> ,
+        :slam_dunks=> ,
       }]
     }
   }
